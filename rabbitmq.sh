ecb8ffaddd8bd2443c7ca11ba085032601a9a3df
Aslam sunkesula <aslam.sukesula@gmail.com>
2023-08-28 15:44:07 +0530

    automating the rabbitmq

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,42 +1,70 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 DATE=$(date +%F)
-LOGSDIR=/tmp
-# /home/centos/shellscript-logs/script-name-date.log
-SCRIPT_NAME=$0
-LOGFILE=$LOGSDIR/$SCRIPT_NAME-$DATE.log
-USERID=$(id -u)
+SCRIPT_NAME="$0"
+LOG_FILE=/tmp/$SCRIPT_NAME-$DATE.log
+
 R="\e[31m"
 G="\e[32m"
-N="\e[0m"
-Y="\e[33m"
+W="\033[0m"
 
-if [ $USERID -ne 0 ];
+if [[ $(id -u) -ne 0 ]]
 then
-    echo -e "$R ERROR:: Please run this script with root access $N"
-    exit 1
+        echo -e "$R ERROR : Please run this sctipt with root user, swich to root and try $W"
+        exit 1
 fi
 
-VALIDATE(){
-    if [ $1 -ne 0 ];
-    then
-        echo -e "$2 ... $R FAILURE $N"
-        exit 1
-    else
-        echo -e "$2 ... $G SUCCESS $N"
-    fi
+VALIDATE()
+{
+    if [[ $? -ne 0 ]]
+        then
+                echo -e "$1 $R ..... Failure $W"
+                exit 2
+        else
+                echo -e "$1 $G ..... Success $W"
+        fi
 }
 
-curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash &>>$LOGFILE
+# Configure YUM Repos from the script provided by vendor
 
-curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash &>>$LOGFILE
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash &>> "$LOG_FILE"
 
-yum install rabbitmq-server -y  &>>$LOGFILE
+VALIDATE $? "Configuring erlnag yum repos"
 
-systemctl enable rabbitmq-server  &>>$LOGFILE
+# Configure YUM Repos for RabbitMQ.
 
-systemctl start rabbitmq-server  &>>$LOGFILE
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash &>> "$LOG_FILE"
 
-rabbitmqctl add_user roboshop roboshop123 &>>$LOGFILE
+VALIDATE $? "Configuring Rabbit MQ YUM Repos"
 
-rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>$LOGFILE
+# Install RabbitMQ
+
+yum install rabbitmq-server -y &>> "$LOG_FILE"
+
+VALIDATE $? "Installing rabbitmq-server"
+
+# Enable and Start RabbitMQ Service
+
+systemctl enable rabbitmq-server &>> "$LOG_FILE"
+
+VALIDATE $?  "Enabling RabbitMQ Service"
+
+systemctl start rabbitmq-server &>> "$LOG_FILE"
+
+VALIDATE $? "Starting RabbitMQ Service"
+
+#RabbitMQ comes with a default username / password as guest/guest. But this user cannot be used to connect. Hence, we need to create one user for the application
+
+rabbitmqctl add_user roboshop roboshop123 &>> "$LOG_FILE"
+
+VALIDATE $? "Adding user roboshop"
+
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>> "$LOG_FILE"
+
+VALIDATE $? "Setting up permissions"
+
+# Validate RabbitMQ up and running and operational
+
+netstat -tulpn | grep 5672 &>> "$LOG_FILE"
+
+VALIDATE $? "RabbitMQ Up and Operational"
